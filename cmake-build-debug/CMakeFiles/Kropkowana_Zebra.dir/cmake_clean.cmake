96993344c88ca0347a3fc231a5a4831a440818d5
Martyna <235887@edu.p.lodz.pl>
2021-02-25 00:12:27 +0100

    01 Robimy metodę policz

---
+++ cmake-build-debug/CMakeFiles/Kropkowana_Zebra.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Kropkowana_Zebra.dir/main.cpp.obj"
+  "Kropkowana_Zebra.exe"
+  "Kropkowana_Zebra.exe.manifest"
+  "Kropkowana_Zebra.pdb"
+  "libKropkowana_Zebra.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Kropkowana_Zebra.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
